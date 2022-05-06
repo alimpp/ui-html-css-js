93cba7b361974a0533dc2d57ce00fd0573cb3f48
ali.mohsenpour <alimpp766@gmail.com>
2022-05-06 14:34:40 -0700

    make ui

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+const goToServices = () => {
+    const services = document.querySelector('.services')
+    services.scrollIntoView()
+}
+
+const goToProtfolio = () => {
+    const protfolio = document.querySelector('.portfolio')
+    protfolio.scrollIntoView()
+}
+
+const goToContact = () => {
+    const contact = document.querySelector('.contact')
+    contact.scrollIntoView()
+}
